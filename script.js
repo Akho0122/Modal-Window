c05a08cc62538646a78cae047aae91bb62097d9d
Akho0122 <mr.akhror.dev@gmail.com>
2022-02-07 17:37:03 +0500

    Refactored

--- script.js
+++ script.js
@@ -5,27 +5,30 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const closeModal = document.querySelector('.close-modal');
 
+const close = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const add = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
 for (let i = 0; i < openModal.length; i++) {
   openModal[i].addEventListener('click', function () {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
+    close();
   });
 }
 
 closeModal.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+  add();
 });
 overlay.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+  add();
 });
 
 document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape') {
-    if (!modal.classList.contains('hidden')) {
-      modal.classList.add('hidden');
-      overlay.classList.add('hidden');
-    }
+    add();
   }
 });
