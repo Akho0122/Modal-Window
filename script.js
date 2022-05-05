b798d07e28179cf652608a523a9cf6edbffec5af
Akho0122 <mr.akhror.dev@gmail.com>
2022-05-05 16:20:32 +0500

    modal javaScript

--- script.js
+++ script.js
@@ -3,25 +3,22 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
-
-const openModal = function () {
+const open = function () {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
-const closeModal = function () {
+const close = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
-for (let i = 0; i < btnOpenModal.length; i++) {
-  btnOpenModal[i].addEventListener('click', openModal);
-}
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+btnOpenModal.forEach((i, el) => i.addEventListener('click', open));
+btnCloseModal.addEventListener('click', close);
+overlay.addEventListener('click', close);
 
 document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape') {
     if (!modal.classList.contains('hidden')) {
-      closeModal();
+      close();
     }
   }
 });
